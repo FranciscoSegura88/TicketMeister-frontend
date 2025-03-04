2e10116fcadbd9fb1356de1920cf9e6a5632f4f4
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2025-03-04 00:22:41 -0600

    feat: implement login and registration components with routing and authentication

---
+++ ticketmeister-client/src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface RegisterData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private API_URL = 'http://localhost:5000';
+
+  constructor(private http: HttpClient) { }
+
+  register(userData: RegisterData): Observable<any> {
+    return this.http.post(`${this.API_URL}/register`, userData);
+  }
+
+  login(credentials: LoginData): Observable<any> {
+    return this.http.post(`${this.API_URL}/login`, credentials);
+  }
+}
